04b07bfee3b5c663e7fccb92847da79471cacc36
Cameron Warner <cameron.w.warner@gmail.com>
2023-11-24 17:16:35 -0500

    Initial Commit

---
+++ Navigation/NavigationApp.swift
@@ -0,0 +1,17 @@
+//
+//  NavigationApp.swift
+//  Navigation
+//
+//  Created by Cameron Warner on 11/24/23.
+//
+
+import SwiftUI
+
+@main
+struct NavigationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
